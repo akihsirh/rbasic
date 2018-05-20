efa20a256e867ac12460914233dbb4df4ce02578
akihsirh <39462103+akihsirh@users.noreply.github.com>
2018-05-20 23:58:57 +0530

    Create typeOfVariables.r

---
+++ typeOfVariables.r
@@ -0,0 +1,23 @@
+#integer
+# L because by default a numeric literal in R is considered a double
+x<- 2L
+typeof(x)
+
+#double
+d <- 2.5
+typeof(d)
+
+#complex
+z <- 3 + 2i
+typeof(z)
+
+#character
+a <- "h"
+typeof(a)
+
+#logical
+t <- TRUE
+typeof(t)
+
+f <- FALSE
+typeof(f)

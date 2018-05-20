28b533ce4dcbff7a5e5b14df773f35910d1e15c0
akihsirh <39462103+akihsirh@users.noreply.github.com>
2018-05-21 00:08:24 +0530

    Create PlayingAroundWithVariables.r

---
+++ PlayingAroundWithVariables.r
@@ -0,0 +1,14 @@
+A <- 10
+B <- 5
+C <- A+B
+C
+
+root <- sqrt(2)
+root
+
+greeting <- "Hello "
+name <- "Name "
+kindness <- "Nice to see you"
+#concatenate the previous pieces of code
+full_sentence <- paste(greeting,name,kindness)
+full_sentence

8c0ce0974977802819d8e5e003f4ada7deddacb4
Hrishika <hrishika.learning@gmail.com>
2018-06-08 11:55:31 +0530

    "Matplot on data subset"

---
+++ Matrices/SubsetMatplot.R
@@ -0,0 +1,40 @@
+
+# implemented using the script and data from the amazing team at www.superdatascience.com
+
+#Seasons
+Seasons <- c("2005","2006","2007","2008","2009","2010","2011","2012","2013","2014")
+
+#Players
+Players <- c("KobeBryant","JoeJohnson","LeBronJames","CarmeloAnthony","DwightHoward","ChrisBosh","ChrisPaul","KevinDurant","DerrickRose","DwayneWade")
+
+#Field Goals
+KobeBryant_FG <- c(978,813,775,800,716,740,574,738,31,266)
+JoeJohnson_FG <- c(632,536,647,620,635,514,423,445,462,446)
+LeBronJames_FG <- c(875,772,794,789,768,758,621,765,767,624)
+CarmeloAnthony_FG <- c(756,691,728,535,688,684,441,669,743,358)
+DwightHoward_FG <- c(468,526,583,560,510,619,416,470,473,251)
+ChrisBosh_FG <- c(549,543,507,615,600,524,393,485,492,343)
+ChrisPaul_FG <- c(407,381,630,631,314,430,425,412,406,568)
+KevinDurant_FG <- c(306,306,587,661,794,711,643,731,849,238)
+DerrickRose_FG <- c(208,208,208,574,672,711,302,0,58,338)
+DwayneWade_FG <- c(699,472,439,854,719,692,416,569,415,509)
+#Matrix
+FieldGoals <- rbind(KobeBryant_FG, JoeJohnson_FG, LeBronJames_FG, CarmeloAnthony_FG, DwightHoward_FG, ChrisBosh_FG, ChrisPaul_FG, KevinDurant_FG, DerrickRose_FG, DwayneWade_FG)
+rm(KobeBryant_FG, JoeJohnson_FG, LeBronJames_FG, CarmeloAnthony_FG, DwightHoward_FG, ChrisBosh_FG, ChrisPaul_FG, KevinDurant_FG, DerrickRose_FG, DwayneWade_FG)
+colnames(FieldGoals) <- Seasons
+rownames(FieldGoals) <- Players
+
+#Subset data -> top 3 players 
+ttfieldgoals <- FieldGoals[1:3,]
+
+#Mat plot
+matplot(t(ttfieldgoals), type = "b", pch = 15:18, col = c(1:4,6))
+legend("bottomleft", inset = 0.001, legend = Players[1:3], col = c(1:4,6) ,pch = 15:18, horiz = F )
+
+#Subset data -> top player
+ttfieldgoals <- FieldGoals[1, , drop=F]
+
+#Mat plot
+matplot(t(ttfieldgoals), type = "b", pch = 15:18, col = c(1:4,6))
+legend("bottomleft", inset = 0.001, legend = Players[1], col = c(1:4,6) ,pch = 15:18, horiz = F )
+
